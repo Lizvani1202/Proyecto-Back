0eaa5562962c158662f5e8eac62dfe52004d78ca
lizbeth-vera-smd <lizbeth.vera@sharemedata.com>
2022-05-22 03:38:49 -0400

    Inicializando Backend

---
+++ proysisgis/src/main/java/bo/ucb/sis/proysisgis/ProysisgisApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.sis.proysisgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProysisgisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProysisgisApplication.class, args);
+	}
+
+}
---
+++ proysisgis/src/test/java/bo/ucb/sis/proysisgis/ProysisgisApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.sis.proysisgis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProysisgisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
